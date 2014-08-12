9524499a978e595586dd5b72673670cdf13acfa6
faruk92 <faruk.cort@ozu.edu.tr>
2014-08-12 16:27:48 +0300

    Initial commit of Mvc party invites

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using  PartyInvites.Models;
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public   ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Evening";
+            return View() ;
+        }
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestresponse)// post ta view döndürme
+        {
+            if (ModelState.IsValid)
+            {
+                //TODO;Email response to the party organizer
+                return View("Thanks", guestresponse);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+    }
+}
---
+++ PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage="Please enter your name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter your email")]
+        [RegularExpression(".+\\@.+\\..+" ,ErrorMessage="please enter an valid email")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Please enter your phone number")]
+        public string phone { get; set; }
+        [Required(ErrorMessage = "Please specify whether you will attend")]
+        public bool? WıllAttend { get; set; }
+    }
+}
